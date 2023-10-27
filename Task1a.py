894a726cd186298a28b18639fd36ae6040fa2bd3
jgp39 <jasmine.papas@duke.edu>
2023-10-27 12:21:10 -0400

    Task 1a complete

---
+++ Task1a.py
@@ -0,0 +1,14 @@
+###Set up
+#save all files to Scrath folder using code below, plus 
+# desired file name
+#outFC= "V:\\ENV859_PS4\\Scratch\\filename"
+
+import arcpy
+#Task 1a
+#set objects
+streams = "V:\\ENV859_PS4\\Data\\streams.shp"
+outFC = "V:\\ENV859_PS4\\Scratch\\StrmBuff1km.shp"
+distance = "1000 meters"
+
+#create buffer 
+arcpy.analysis.Buffer(streams,outFC,distance,"","","ALL")
