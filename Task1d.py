79f853f35fdfa803b487168e355179d122228965
jgp39 <jasmine.papas@duke.edu>
2023-11-01 17:24:33 -0400

    IDK I QUIT

--- Task1d.py
+++ Task1d.py
@@ -1,11 +1,25 @@
 #import modules
-import arcpy
+import arcpy, os, sys
 
+arcpy.env.workspace = "V:\\ENV859_PS4\\Scratch"
+
+#allow output files to be overwritten
 arcpy.env.overwriteOutput = True
 
-#set objects
+
+#set variables
 streams = "V:\\ENV859_PS4\\Data\\streams.shp"
-distance = arcpy.GetParameterAsText()
+
+
+for i in range (5):
+    distance = (sys.argv[1]),
+    outFC = str("V:\\ENV859_PS4\\Scratch\\buff_"+ sys.argv[3] +"m.shp"),
+    print(distance)
+
+
 
 #create buffer 
 arcpy.analysis.Buffer(streams,outFC,distance,"","","ALL")
+
+
+print(arcpy.GetMessages())
---
+++ Task2.py
@@ -0,0 +1,7 @@
+import arcpy, sys, os
+
+arcpy.env.OverwriteOutput = True
+
+roads = "V:\\ENV859_PS4\\Data\\Roads.shp"
+strings = "0:
+
