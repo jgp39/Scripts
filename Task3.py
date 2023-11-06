41e099b0eaf0dc73256c454a03c28420627e96b1
jgp39 <jasmine.papas@duke.edu>
2023-11-06 13:53:57 -0500

    FINALLY finished Task 3

--- Task3.py
+++ Task3.py
@@ -7,15 +7,16 @@ arcpy.env.workspace= "V:\\ENV859_PS4\\Data"
 if arcpy.CheckProduct("ArcInfo") =="Available":
     print("ArcPy is using ArcInfo")
 else:
-        print("Proper licensing is not available.")
+    print("Proper licensing is not available.")
 
 #create a list with all BMR feature classes
 BMR = arcpy.ListFeatureClasses("BMR*")
 
+output_FC= "V:\\ENV859_PS4\\Scratch"
 
 #Loop through BMR list to create new folders
 for file in BMR:
-    output_FC= f"V:\\ENV859_PS4\\Scratch\\{file}"
-    arcpy.CreateFolder_management(output_FC, file)
-    split_FC= 
-    arcpy.Split_analysis(BMR(), split_FC, output_FC, "TriCounties")
+    filename= file[:-4]
+    arcpy.management.CreateFolder(output_FC, filename)
+    input_FC= file
+    arcpy.Split_analysis(input_FC,"V:\\ENV859_PS4\\Data\\TriCounties.shp", "CO_NAME", output_FC, )
